a5e04a58710722faa96ce0a016f50a772ec0b0c2
Precious <Preshomondi@gmail.com>
2022-06-27 00:09:10 +0300

    commit tree 7cead13b02cadc8ebb01ab912ee5ce794b0a1724

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
 /* Your Code Here */
+let createEmployeeRecord = function(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+let createEmployeeRecords = function(mainArr) {
+    return mainArr.map(function(arr){
+        return createEmployeeRecord(arr)
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10), date
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10), date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(newDate){
+    let inTime = this.timeInEvents.find(function(e){
+        return e.date === newDate
+    })
+
+    let outTime = this.timeOutEvents.find(function(e){
+        return e.date === newDate
+    })
+
+    return (outTime.hour - inTime.hour) / 100
+}
+
+let wagesEarnedOnDate = function(newDate) {
+    let wage = hoursWorkedOnDate.call(this, newDate) * this.payPerHour
+    return parseFloat(wage.toString())
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +74,13 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function(arr1, firstName) {
+    return arr1.find(function(r){
+        return r.firstName === firstName
+    })
+}
+let calculatePayroll = function(recordsArr){
+    return recordsArr.reduce(function(memo, r){
+        return memo + allWagesFor.call(r)
+    }, 0)
+}
